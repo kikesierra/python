2eabd1d083f70dacabc43e46d7a2a6721aae16d8
Enrique Sierra <esierrab@gmail.com>
2020-07-28 13:54:32 +0200

    Cambios menores

--- leccion1.py
+++ leccion1.py
@@ -2,45 +2,71 @@
 # Conceptos básicos de Pyhon #
 # ---------------------------#
 
-#Tipos de variables (int, float, str, bool)
-vEntero = 2 #Variable de tipo int
-vDecimal = 2.5 #Variable de tipo float
-vCadena1 = "Texto" #Variable de tipos str con comillas dosbles
-vCadena2 = 'Otro texto' #Variable de tipos str con comillas simples
-vCadena3 = """Otro texto largo con caracteres especiales
-              espacios
-              y saltos de linea""" #Variable de tipos str con comillas triples
-vResultadoT = True #Variable de tipo bool
-vResultadoF = False 
-vLista1 = [1,2,3,4] #Lista de números enteros
-vLista2 = [1, 2.5, 'Texto', True, [1,2]] #Lista de valores de tipos diferentes
+# Tipos de variables (int, float, str, bool)
+vSinTipo = None  # Variable sin valor asignado y por lo tanto sin tipo conocido
+vEntero = 2  # Variable de tipo int
+vDecimal = 2.5  # Variable de tipo float
+vCadena1 = "Texto"  # Variable de tipos str con comillas dosbles
+vCadena2 = 'Otro texto'  # Variable de tipos str con comillas simples
+vCadena3 = """Otro texto largo con caracteres especiales,'", tabuladores
+              espacios y
+              saltos de linea"""  # Variable de tipos str con comillas triples
 
-#Operaciones aritméticas
+print(vCadena3)
+
+vResultadoT = True  # Variable de tipo bool
+vResultadoF = False
+vLista1 = [1, 2, 3, 4]  # Lista de números enteros
+vLista2 = [1, 2.5, 'Texto', True, [1, 2]]  # Lista de valores de tipos diferentes
+
+# Operaciones aritméticas
 suma = vEntero + vDecimal
 resta = vDecimal - vEntero
 producto = vEntero * vDecimal
 potencia = vDecimal ** vEntero
-divisionP = vDecimal / vEntero #División precisa
-divisionE = vDecimal // vEntero #División entera
-resto = vDecimal % vEntero #Resto de la división
+divisionP = vDecimal / vEntero  # División precisa
+divisionE = vDecimal // vEntero  # División entera
+resto = vDecimal % vEntero  # Resto de la división
 
-#Operaciones con cadenas
-concatena = vCadena1 + vCadena2 #Une las cadenas
-longitud = len(vCadena1) #Longitud de la cadena
-minusculas = vCadena1.lower() #Devuelve la cadena en minúsculas
-mayusculas = vCadena1.upper() #Devuelve la cadena en mayúsculas
+# Operaciones con cadenas
+concatena = vCadena1 + vCadena2  # Une las cadenas
+longitud = len(vCadena1)  # Longitud de la cadena
+minusculas = vCadena1.lower()  # Devuelve la cadena en minúsculas
+mayusculas = vCadena1.upper()  # Devuelve la cadena en mayúsculas
+concatena = vCadena1 * 4  # Concatena 4 veces la cadena1
 
-primerCaracter = vCadena1[0] #Se puede recorrer la cadena de 0 a len-1
-ultimoCaracter = vCadena1[-1] #Se recorre la cadena al reves: -1, -2, -3,....
+# Obtener partes de una cadena
+trozo = vCadena2[0]  # Obtiene el primer caracter de lacadena
+trozo = vCadena2[-1]  # Obtiene el último caracter de la cadena
+trozo = vCadena2[1:3]  # Obtiene desde la posición 1 hasta la 3
+trozo = vCadena2[3:]  # Obtiene desde la posición 3 hasta el final de la cadena
+trozo = vCadena2[:5]  # Obtiene desde el principio hasta la posición 5
 
-#Capturar entrada de teclado - Siempre se recoge como str
+
+primerCaracter = vCadena1[0]  # Se puede recorrer la cadena de 0 a len-1
+ultimoCaracter = vCadena1[-1]  # Se recorre la cadena al reves: -1, -2, -3,....
+
+# Capturar entrada de teclado - Siempre se recoge como str
 entrada1 = input()
-entrada2 = input('Dime tu nombre: ') #Indicando un mensaje previo
+entrada2 = input('Dime tu nombre: ')  # Indicando un mensaje previo
 
-#Salida por pantalla
-print(entrada1) #Salida sencilla
-print("El valor de la variable entrada es: ", entrada1) #Imprimir con texto fijo
+# Ejemplos de Salida por pantalla
+# Salida sencilla
+print(entrada1)
+# Salida con texto fijo
+print("El valor de la variable entrada es: ", entrada1)
+# Salida con varios textos y variables separadas por comas
 print("Entrada 1: ", entrada1, " - Entrada 2: ", entrada2)
+# Salida usando la opción f y las variables entre corchetes
 print(f'La variable entrada 1 es {entrada1} y la variable entrada 2 es {entrada2}')
 
+# Obtener el tipo de una variable
+print(f"""Mis variables son de los siguientes tipos:
+         vSinTipo    = {type(vSinTipo)}
+         vEntero     = {type(vEntero)}
+         vDecimal    = {type(vDecimal)}
+         vCadena1    = {type(vCadena1)}
+         vLista1     = {type(vLista1)}
+         vResultadoT = {type(vResultadoT)}""")
 
+# Convertir las entradas de teclado (de tipo str) al tipo deseado (int, float)
