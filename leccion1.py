8ba3cbbe107222284a883d67943c812414cf3588
Enrique Sierra <esierrab@gmail.com>
2020-07-28 10:09:33 +0200

    Añadido entrada y salida de datos

--- leccion1.py
+++ leccion1.py
@@ -1,6 +1,6 @@
-#
-#Conceptos básicos de Pyhon
-#
+# ---------------------------#
+# Conceptos básicos de Pyhon #
+# ---------------------------#
 
 #Tipos de variables (int, float, str, bool)
 vEntero = 2 #Variable de tipo int
@@ -29,5 +29,18 @@ concatena = vCadena1 + vCadena2 #Une las cadenas
 longitud = len(vCadena1) #Longitud de la cadena
 minusculas = vCadena1.lower() #Devuelve la cadena en minúsculas
 mayusculas = vCadena1.upper() #Devuelve la cadena en mayúsculas
+
+primerCaracter = vCadena1[0] #Se puede recorrer la cadena de 0 a len-1
+ultimoCaracter = vCadena1[-1] #Se recorre la cadena al reves: -1, -2, -3,....
+
+#Capturar entrada de teclado - Siempre se recoge como str
+entrada1 = input()
+entrada2 = input('Dime tu nombre: ') #Indicando un mensaje previo
+
+#Salida por pantalla
+print(entrada1) #Salida sencilla
+print("El valor de la variable entrada es: ", entrada1) #Imprimir con texto fijo
+print("Entrada 1: ", entrada1, " - Entrada 2: ", entrada2)
+print(f'La variable entrada 1 es {entrada1} y la variable entrada 2 es {entrada2}')
 
 
