482984d11036e60f9dcfedaf7ad990324d179966
Enrique Sierra <esierrab@gmail.com>
2020-07-29 16:59:07 +0200

    Uso de módulos

--- leccion6_2.py
+++ leccion6_2.py
@@ -1,8 +1,14 @@
 # Programa que importa funciones de un módulos
 from misfunciones import saludar  # Se importa solo la función saludar
 
+# Se puede importar una función de un módulo y renombrarla (alias)
+from misfunciones import despedirse as adios
+
 # Para usar la función ya no se usa el nombre dle módulo, sólo el de la función
 saludar('Kike')
 
 nombre = input('Dime tu nombre: ')
 saludar(nombre)
+
+# Llamar a la función importada con el alias
+adios(nombre)
---
+++ leccion7.py
@@ -0,0 +1,8 @@
+# --------------------------------------
+# PIP: Gestor de módulos de Python
+#    Sirve para instalar módulos que necesitamos en nuestras aplicaciones y que usaremos con import
+#
+# Podemos ver en la terminal:
+#   - la versión de pip: pip --version
+#   - Los módulos instalados: pip list
+#   - Instalar un módulo: pip install nombreModulo
