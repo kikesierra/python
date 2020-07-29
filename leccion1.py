81e7692d0ee1fb4087cfcb5e5948f303ca7c59e3
Enrique Sierra <esierrab@gmail.com>
2020-07-29 11:01:03 +0200

    Nuevas opciones de formato

--- leccion1.py
+++ leccion1.py
@@ -33,6 +33,7 @@ concatena = vCadena1 + vCadena2  # Une las cadenas
 longitud = len(vCadena1)  # Longitud de la cadena
 minusculas = vCadena1.lower()  # Devuelve la cadena en minúsculas
 mayusculas = vCadena1.upper()  # Devuelve la cadena en mayúsculas
+tokeniza = vCadena1.split()  # Devuelve una lista con las palabras de la cadena
 concatena = vCadena1 * 4  # Concatena 4 veces la cadena1
 
 # Obtener partes de una cadena
@@ -53,12 +54,23 @@ entrada2 = input('Dime tu nombre: ')  # Indicando un mensaje previo
 # Ejemplos de Salida por pantalla
 # Salida sencilla
 print(entrada1)
+
 # Salida con texto fijo
 print("El valor de la variable entrada es: ", entrada1)
+
 # Salida con varios textos y variables separadas por comas
 print("Entrada 1: ", entrada1, " - Entrada 2: ", entrada2)
+
 # Salida usando la opción f y las variables entre corchetes
 print(f'La variable entrada 1 es {entrada1} y la variable entrada 2 es {entrada2}')
+
+# Forma alternativa de usar el format
+print('La variable entrada1 es {} y la variable entrada2 es {}'.format(entrada1, entrada2))
+
+# Usando el format para dar formato a números
+numero = 1300 / 3
+print('El número sin formato es {_1}'.format(_1=numero))
+print('El número con formato es {v:1.2f}'.format(v=numero))  # Parte entera y dos decimales
 
 # Obtener el tipo de una variable
 print(f"""Mis variables son de los siguientes tipos:
@@ -70,3 +82,17 @@ print(f"""Mis variables son de los siguientes tipos:
          vResultadoT = {type(vResultadoT)}""")
 
 # Convertir las entradas de teclado (de tipo str) al tipo deseado (int, float)
+edad = input('Dime tu edad: ')
+edad = int(edad)
+
+peso = input('Dime tu peso: ')
+peso = float(peso)
+
+# Funciones sobre cadenas
+cadena = input('Dime tu nombre y apellidos: ')
+cadenaL = cadena.split()  # Devuelve una lista con las palabras de la cadena
+print(cadenaL)
+
+# Split admite el caracter separador de las palabras
+cadena = 'SIERRA*BLANCO*ENRIQUE'
+print(cadena.split('*'))
